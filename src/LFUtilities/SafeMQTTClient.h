eae0fafc831f8f5e679c225a756d3a46b9e2a272
destroyedlolo <l_faillie@yahoo.com>
2018-06-28 01:41:19 +0200

    Add connect()

--- src/LFUtilities/SafeMQTTClient.h
+++ src/LFUtilities/SafeMQTTClient.h
@@ -8,31 +8,43 @@
 #ifndef SAFEMQTTCLIENT_H
 #define SAFEMQTTCLIENT_H	0.0100
 
-#include <KeepInRTC.h>
 #include <PubSubClient.h>
 
-#define SMC_RETRY_DELAY 500	// Delay b/w attempts (ms)
+#include <LFUtilities/Duree.h>
 
-class SafeMQTTClient : public KeepInRTC::KeepMe {
-	struct {
-		uint32_t misses;	// number of failled connection
-	} data_to_keep;
+#define SMC_RETRY_DELAY 100	// Delay b/w attempts (ms)
 
+class SafeMQTTClient {
 	PubSubClient clientMQTT;
 
 public:
 	SafeMQTTClient( 
-		KeepInRTC &kir, // RTC context
 		Client &client	// Network client
 	) : 
-		KeepInRTC::KeepMe( kir, (uint32_t *)&this->data_to_keep, sizeof(this->data_to_keep) ),
 		clientMQTT( client )
-	{
-		if( !kir.isValid() ){	// nothing in RTC, set default values
-			this->data_to_keep.misses = 0;
-			this->save();
+	{ }
+
+	bool connect( 
+		const char *client_name, 
+		Duree &duration,
+		unsigned long int maxtries=1500,	// how long we will try ? (mS)
+		bool clear_session=true
+	){
+		duration.reInit();
+		for(;;){
+			if( this->clientMQTT.connect( client_name, clear_session ) ){	// Connected
+				duration.Fini();
+				return true;
+			} else {	// Failure
+				/*D* Peut-être ajouter un callback pour afficher l'erreur */
+				if( *duration > maxtries ) // Too long
+					return false;
+				delay( SMC_RETRY_DELAY );
+			}
 		}
 	}
+
+	PubSubClient &getClient( void ){ return this->clientMQTT; }
 };
 
 #endif
