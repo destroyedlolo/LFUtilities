1b71765c4a638e3b5c14fbcd68a38ce131786a12
destroyedlolo <l_faillie@yahoo.com>
2018-06-29 00:42:06 +0200

    Publish MQTT connection

--- src/LFUtilities/SafeMQTTClient.h
+++ src/LFUtilities/SafeMQTTClient.h
@@ -6,7 +6,7 @@
  *	27/06/2018 First version
  */
 #ifndef SAFEMQTTCLIENT_H
-#define SAFEMQTTCLIENT_H	0.0100
+#define SAFEMQTTCLIENT_H	0.0101
 
 #include <PubSubClient.h>
 
@@ -16,13 +16,18 @@
 
 class SafeMQTTClient {
 	PubSubClient clientMQTT;
+	String mqtttopic;	// topic to publish mqtt connection duration to
 
 public:
 	SafeMQTTClient( 
-		Client &client	// Network client
+		Client &client,		// Network client
+		const char *atopic	// root of topics to publish too (if empty, doesn't publish)
 	) : 
 		clientMQTT( client )
-	{ }
+	{
+		if( atopic )
+			mqtttopic = String( atopic );
+	}
 
 	bool connect( 
 		const char *client_name, 
@@ -34,6 +39,8 @@ public:
 		for(;;){
 			if( this->clientMQTT.connect( client_name, clear_session ) ){	// Connected
 				duration.Fini();
+				if( mqtttopic.length() )	// Publish connection duration
+					this->clientMQTT.publish( mqtttopic.c_str(), String(*duration, DEC).c_str() );
 				return true;
 			} else {	// Failure
 				/*D* Peut-être ajouter un callback pour afficher l'erreur */
