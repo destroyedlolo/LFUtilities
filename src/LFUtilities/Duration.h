35ea5d665509059d50e59d0bffe8d1aded1bcbfd
destroyedlolo <l_faillie@yahoo.com>
2018-06-30 12:05:34 +0200

    Correct variables' type

--- src/LFUtilities/Duration.h
+++ src/LFUtilities/Duration.h
@@ -5,16 +5,16 @@
  * 30/06/2018 Back in english (as put in a library)
  */
 #ifndef DURATION_H
-#define DURATION_H	0.0200
+#define DURATION_H	0.0201
 
 class Duration {
-	unsigned long int start, end;
+	uint32_t start, end;
 
 	public:
 		Duration( void ) { reInit(); }
 		void reInit( void ) { start = millis(); end = 0; }
-		unsigned long int Finished( void ){ end = millis(); return (end - start); }
-		unsigned long int operator *( void ){ return( (end ? end : millis()) - start ); }
+		uint32_t Finished( void ){ end = millis(); return (end - start); }
+		uint32_t operator *( void ){ return( (end ? end : millis()) - start ); }
 };
 
 #endif
