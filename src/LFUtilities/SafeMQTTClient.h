09f917eb92f1fb25ff7c97895f846a0cfb2ab348
destroyedlolo <l_faillie@yahoo.com>
2018-06-30 19:42:37 +0200

    MQTT working

--- src/LFUtilities/SafeMQTTClient.h
+++ src/LFUtilities/SafeMQTTClient.h
@@ -27,6 +27,8 @@ class SafeMQTTClient {
 	String MQTT_Message;	// Information topics
 	String MQTT_WiFi;		// Wifi connection duration
 	String MQTT_MQTT;		// MQTT connection duration
+	uint32_t mqtt_maxtries;	// For howlong we're trying to connect to the broker
+	bool mqtt_clear_session;	// Clear session when connecting to MQTT ?
 
 	String WSSID;
 	String WPWD;
@@ -35,11 +37,15 @@ public:
 	SafeMQTTClient( 
 		Client &client,		// Network client
 		const char *assid, const char *apwd,	// WiFi
+		const char *abURL, int abport,	// MQTT Broker
 		const char *aclientID,	// MQTT CLientID
-		const char *atopic	// root of topics to publish too (if empty, doesn't publish)
+		const char *atopic = NULL,	// root of topics to publish too (if empty, doesn't publish)
+		bool clear_session = false
 	) : 
 		clientMQTT( client ),
 		clientID( aclientID ),
+		mqtt_maxtries( SMC_MQTT_MAX_RETRY ),
+		mqtt_clear_session( clear_session ),
 		WSSID( assid), WPWD(apwd)
 	{
 		if( atopic ){
@@ -48,9 +54,15 @@ public:
 			this->MQTT_WiFi = this->mqtttopic + "WiFi";
 			this->MQTT_MQTT = this->mqtttopic + "MQTT";
 		}
+
+		this->clientMQTT.setServer( abURL, abport );
 	}
 
-		/* Networks */
+
+		/*******
+		 * Networks 
+		 *******/
+
 	bool connectWiFi(
 		Duration &duration,
 		uint32_t maxtries=SMC_WIFI_MAX_RETRY
@@ -61,9 +73,10 @@ public:
 			if( WiFi.status() == WL_CONNECTED ){
 				duration.Finished();
 #ifdef SERIAL_ENABLED
-				Serial.print("\nok : ");
+				Serial.print("\nWiFi ok : ");
 				Serial.println( *duration );
 #endif
+				this->publish( this->MQTT_WiFi, *duration, true );
 				return true;
 			}
 
@@ -77,26 +90,36 @@ public:
 		}
 	}
 
-#if 0
-		/* MQTT */
+		/*********
+		 * MQTT
+		 *********/
+	void setMQTTMaxTries( uint32_t val ){ this->mqtt_maxtries = val; }
+	uint32_t getMQTTMaxTries( void ){ return this->mqtt_maxtries; }
+
+	bool connected( void ){ return this->clientMQTT.connected(); }
+
 	bool connectMQTT( 
-		const char *client_name, 
-		Duration &duration,
-		uint32_t maxtries=SMC_MQTT_MAX_RETRY,
-		bool clear_session=true
+		Duration &duration
 	){
 		duration.reInit();
 		for(;;){
-			if( this->clientMQTT.connect( client_name, clear_session ) ){	// Connected
+			if( this->clientMQTT.connect( this->clientID.c_str(), this->mqtt_clear_session ) ){	// Connected
 				duration.Finished();
-				if( mqtttopic.length() )	// Publish connection duration
-					this->clientMQTT.publish( mqtttopic.c_str(), String(*duration, DEC).c_str() );
+#ifdef SERIAL_ENABLED
+				Serial.print("\nMQTT ok : ");
+				Serial.println( *duration );
+#endif
+				if( this->mqtttopic.length() )	// Publish connection duration
+					this->clientMQTT.publish( MQTT_MQTT.c_str(), String(*duration, DEC).c_str());
 				return true;
 			} else {	// Failure
 				/*D* Peut-être ajouter un callback pour afficher l'erreur */
-				if( *duration > maxtries ) // Too long
+				if( *duration > this->mqtt_maxtries ) // Too long
 					return false;
 				delay( SMC_RETRY_DELAY );
+#ifdef SERIAL_ENABLED
+			Serial.print(">");
+#endif
 			}
 		}
 	}
@@ -104,16 +127,34 @@ public:
 	PubSubClient &getClient( void ){ return this->clientMQTT; }
 
 		/* Publish messages */
-	void logMsg( const char *msg ){
-		this->publish( this->MQTT_Message, msg );
+	void publish(
+		String &topic, const char *msg,	// What to publish
+		bool reconnect=true 			// reconnect if the network is lost
+	){
+		if( !this->clientMQTT.connected() ){
+			if( reconnect ){
+				Duration dMQTT;
+				if( !this->connectMQTT( dMQTT ) )	// Re-connection failed
+					return;
+			} else	// Unable to reconnect
+				return;
+		}
+
+		this->clientMQTT.publish( topic.c_str(), msg );
+	}
+
+	void publish( String &topic, const uint32_t val, bool reconnect=true ){
+		this->publish( topic, String(val, DEC).c_str(), reconnect );
+	}
+
+	void logMsg( const char *msg, bool reconnect=true ){
+		this->publish( this->MQTT_Message, msg, reconnect );
 #	ifdef SERIAL_ENABLED
-		if(Serial)
-			Serial.println( msg );
-#endif
+		Serial.println( msg );
+#	endif
 	}
 
 	void logMsg( String &msg ){ this->logMsg( msg.c_str() ); }
-#endif
 };
 
 #endif
